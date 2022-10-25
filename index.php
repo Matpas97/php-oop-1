e6fb0b4759a9a770158e13d8bc51a2e92694c33f
Matpas97 <passarellim22@gmail.com>
2022-10-25 16:34:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// create un file index.php in cui:
+// - è definita una classe ‘Movie’
+ //  => all'interno della classe sono dichiarate delle variabili d'istanza
+ //  => all'interno della classe è definito un costruttore
+  // => all'interno della classe è definito almeno un metodo
+// - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
+
+class Movie {
+    public $name;
+    
+}
+// Variabili d'Istanza
+$watcher = new Movie();
+$maigret = new Movie();
+$siccità = new Movie();
+
+$watcher->name = "Watcher";
+$maigret->name = "Maigret";
+$siccità->name = "Siccità";
+
+var_dump($watcher);
+var_dump($maigret);
+var_dump($siccità);
+
+echo "$watcher";
+echo "$maigret";
+echo "$siccità";
